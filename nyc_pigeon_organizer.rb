3905b8979d621ce173b270555023106b1b264534
adam2222 <adam222@comcast.net>
2016-07-12 18:45:31 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'byebug'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = {}
+  pigeon_names = []
+
+  pigeon_names = data[:gender][:male] + data[:gender][:female]
+  
+  pigeon_names.each do |name|
+    organized_hash[name] = {}
+  end
+
+  data.each do |attribute, attribute_data|
+    attribute_data.each do |attribute_value, names|
+
+      names.each do |name|
+
+        if organized_hash[name][attribute]
+          organized_hash[name][attribute] << attribute_value.to_s
+        else
+          organized_hash[name][attribute] = [attribute_value.to_s]
+        end
+
+      end
+    end
+  end
+
+  organized_hash
 end
